573a7516ac7bf8c8452e009c063070479155191b
Kohei Suzuki <koheisuzuki@me.com>
2023-07-24 12:20:45 +0200

    correct structure

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost',
-    port: 3000,
     proxy: {
       '/api/': {
         target: 'http://localhost:8080',
