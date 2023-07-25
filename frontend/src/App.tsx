d846751d8d0cc5c442c5818adfcfedb7fcab5e45
Kohei Suzuki <koheisuzuki@me.com>
2023-07-25 17:36:37 +0200

    Add PlayingCard.ts and axios in App.tsx in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,36 @@
+import {useEffect, useState} from "react";
+import {PlayingCard} from "./model/PlayingCard.ts";
+import axios from "axios"
+
 export default function App() {
 
-  return (
-    <>
-      Hallo World
-    </>
-  )
+    const [playingCards, setPlayingCards] = useState<PlayingCard[]>([]);
+    const gameSize = "large";
+
+    function loadPlayingCards() {
+        axios.get(
+            "api/playing_cards/" + gameSize)
+
+            .then((response) => {
+                    setPlayingCards(response?.data)
+                }
+            )
+            .catch(console.error)
+    }
+
+    useEffect(
+        () => {
+            loadPlayingCards()
+        }, []
+    )
+
+
+    return (
+        <>
+
+            <h2>Concentration</h2>
+            {playingCards.map((playingCard: PlayingCard) => <div key={playingCard}> {playingCard}</div>)}
+
+        </>
+    );
 }
