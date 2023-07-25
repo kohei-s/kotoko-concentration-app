d71b4d18ac56823241de8b363b3a85c1584f7b77
Kohei Suzuki <koheisuzuki@me.com>
2023-07-25 22:00:11 +0200

    Add lombok.config and jacoco in pom.xml, modify BackendApplicationTests

--- backend/src/test/java/de/neuefische/koheis/backend/BackendApplicationTests.java
+++ backend/src/test/java/de/neuefische/koheis/backend/BackendApplicationTests.java
@@ -2,12 +2,12 @@ package de.neuefische.koheis.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() {assertTrue(true);
     }
 
 }
