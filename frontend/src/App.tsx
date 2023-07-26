884bca942c93bb04bed1b6326b24fe84ef175865
Kohei Suzuki <koheisuzuki@me.com>
2023-07-26 13:24:09 +0200

    Add CharacterCard.ts and get methode in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,31 @@
 import {useEffect, useState} from "react";
 import axios from "axios"
+import {CharacterCard} from "./CharacterCard.ts";
 
 export default function App() {
 
-
     const [playingCards, setPlayingCards] = useState<string[]>([""]);
+    const [characterCards, setCharacterCards] = useState<CharacterCard[]>([]);
     const gameSize = "large";
 
     function loadPlayingCards() {
-         axios.get<string[]>(
+        axios.get<string[]>(
             "api/playing_cards/" + gameSize)
             .then((response) => {
-                    setPlayingCards(response.data)
-                })
-             .catch(console.error)
+                setPlayingCards(response.data)
+            })
+            .catch(console.error)
     }
+
+    function loadCharacterCards() {
+        axios.get<CharacterCard[]>(
+            "api/playing_cards/cards")
+            .then((response) => {
+                setCharacterCards(response?.data)
+            })
+            .catch(console.error)
+    }
+
 
     useEffect(
         () => {
@@ -25,8 +36,15 @@ export default function App() {
     return (
         <>
             <h2>Concentration</h2>
-            {playingCards.map((playingCard: string, index) =>
-                <div key={index}>{playingCard}</div>)}
+            <div>
+                {playingCards.map((playingCard: string) =>
+                    <div key={playingCard}>{playingCard}</div>)}
+            </div>
+            <div>
+                <button onClick={loadCharacterCards}>CharacterCards</button>
+                {characterCards.map((characterCard: CharacterCard) =>
+                    <div key={characterCard.id}>{characterCard.character}</div>)}
+            </div>
         </>
     );
 }
