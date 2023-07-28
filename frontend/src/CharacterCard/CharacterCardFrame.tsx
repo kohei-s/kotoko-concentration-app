daf0e48a5b995e080158735d24117b51e20f8af5
Kohei Suzuki <koheisuzuki@me.com>
2023-07-28 14:52:48 +0200

    Add updateCharacterCard in frontend, modify ui after group project, add css files and dependencies accordingly

--- frontend/src/CharacterCard/CharacterCardFrame.tsx
+++ frontend/src/CharacterCard/CharacterCardFrame.tsx
@@ -1,13 +1,19 @@
 import {CharacterCard} from "./CharacterCard.ts";
+import {Card, CardContent, Typography} from "@mui/material";
 
 type Props = {
     characterCard: CharacterCard,
+    update: () => void
 }
-export default function CharacterCardFrame(props: Props){
+export default function CharacterCardFrame(props: Props) {
 
     return (
-        <div>
-            {props.characterCard.character}
-        </div>
+        <Card onClick={props.update} className={"characterCardFrame"}>
+            <CardContent>
+                <Typography>
+                    {props.characterCard.character}
+                </Typography>
+            </CardContent>
+        </Card>
     );
 }
