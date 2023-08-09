02b15f68aa54cd967f56936866ed0a310a432d26
Kohei Suzuki <koheisuzuki@me.com>
2023-08-09 23:06:56 +0200

    Modify game size definition in backend

--- backend/src/main/java/de/neuefische/koheis/backend/gamegcards/GameCardsService.java
+++ backend/src/main/java/de/neuefische/koheis/backend/gamegcards/GameCardsService.java
@@ -34,22 +34,31 @@ public class GameCardsService {
 
         return gameCardList.stream().filter(card -> cardSetName.equals(card.getCardSetName())).toList();
     }
+    
+    public int[] setIndexOfRowAndColumn (String gameSize){
+        int[] rowAndColumn = new int[2];
+        if (gameSize.equals("large")){
+            rowAndColumn[0] = 4;
+            rowAndColumn[1] = 4;
+        } else if (gameSize.equals("medium")){
+            rowAndColumn[0] = 3;
+            rowAndColumn[1] = 4;
+        } else {
+            rowAndColumn[0] = 3;
+            rowAndColumn[1] = 3;
+        }
 
-    // "small": 2 names & 6 numbers / "medium": 2 names & 13 numbers / "large": 4 names & 13 numbers
+        return  rowAndColumn;
+    }
+
     public GameCardsGrid getGameCardsGrid(String gameSize, String cardSetName) {
-
         List<GameCard> gameCardList = findByCardSetName(cardSetName);
         GameCard[][] gameCardBoard = generateGameBoard(gameSize, gameCardList);
+        int[] rowAndColumn = setIndexOfRowAndColumn(gameSize);
+        boolean[][] isMatched = new boolean[rowAndColumn[0]][rowAndColumn[1]];
 
-        int indexOfRow;
-        switch (gameSize) {
-            case "large" -> indexOfRow = 13;
-            case "small" -> indexOfRow = 3;
-            default -> indexOfRow = 7;
-        }
-        boolean[][] isMatched = new boolean[indexOfRow][4];
-        for (int i = 0; i < indexOfRow; i++) {
-            for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < rowAndColumn[0]; i++) {
+            for (int j = 0; j < rowAndColumn[1]; j++) {
                 isMatched[i][j] = false;
             }
         }
@@ -58,24 +67,24 @@ public class GameCardsService {
     }
 
     public GameCard[][] generateGameBoard(String gameSize, List<GameCard> allGameCards) {
-        int indexOfRow;
+        int pairing;
         switch (gameSize) {
-            case "large" -> indexOfRow = 13;
-            case "small" -> indexOfRow = 3;
-            default -> indexOfRow = 7;
+            case "small" -> pairing = 4;
+            case "medium" -> pairing = 6;
+            default -> pairing = 8;
         }
-        GameCard[][] gameCardGrid = new GameCard[indexOfRow][4];
-        List<GameCard> gameCards = new ArrayList<>(allGameCards.subList(0, indexOfRow * 2));
+        int[] rowAndColumn = setIndexOfRowAndColumn(gameSize);
+        GameCard[][] gameCardGrid = new GameCard[rowAndColumn[0]][rowAndColumn[1]];
+        List<GameCard> gameCards = new ArrayList<>(allGameCards.subList(0, pairing * 2));
         gameCards.addAll(List.copyOf(gameCards));
         GameCard emptyCard = new GameCard(idService.createRandomId(), "empty", "empty");
-        if (gameSize.equals("medium")) {
-            gameCards.add(emptyCard);
+        if (gameSize.equals("small")) {
             gameCards.add(emptyCard);
         }
         shuffle(gameCards);
         int k = 0;
-        for (int i = 0; i < indexOfRow; i++) {
-            for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < rowAndColumn[0]; i++) {
+            for (int j = 0; j < rowAndColumn[1]; j++) {
                 gameCardGrid[i][j] = gameCards.get(k);
                 k++;
             }
@@ -85,16 +94,19 @@ public class GameCardsService {
     }
 
     public GameCard addGameCard (GameCardWithoutId gameCardWithoutId) {
+
         return gameCardsRepository.insert(new GameCard(idService.createRandomId(), gameCardWithoutId.getTitle(), gameCardWithoutId.getCardSetName()));
     }
 
     public GameCard updateGameCard(GameCardWithoutId gameCardWithoutId, String id){
         if (!gameCardsRepository.existsById(id)) throw new NoSuchElementException();
+
         return gameCardsRepository.save(new GameCard(id, gameCardWithoutId.getTitle(), gameCardWithoutId.getCardSetName()));
     }
 
     public void deleteGameCard(String id){
         if (!gameCardsRepository.existsById(id)) throw new NoSuchElementException();
+
         gameCardsRepository.deleteById(id);
     }
 
