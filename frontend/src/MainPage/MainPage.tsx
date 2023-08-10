0164b9ae30b8998ff8395989855407e9ae64e3ef
Kohei Suzuki <koheisuzuki@me.com>
2023-08-10 10:46:01 +0200

    Refactor main page in frontend

--- frontend/src/MainPage/MainPage.tsx
+++ frontend/src/MainPage/MainPage.tsx
@@ -1,32 +1,12 @@
-import {Link} from "react-router-dom";
 import "./MainPage.css"
-import {Button, Stack} from "@mui/material";
+import GameMenu from "../GameMenu/GameMenu.tsx";
 
 export default function MainPage() {
 
 
     return (
         <>
-            <div className={"start"}>
-                <img height={"290vm"} src="/logos/kotoko-logo.png" alt="start"/>
-            </div>
-            <Stack direction="row" spacing={0.5} justifyContent="center">
-            <Button variant="contained" sx={{background:"#D05F5F", boxShadow:0, borderRadius:'20px'}}>
-                <Link to="/hiragana">あ</Link>
-            </Button>
-            <Button variant="contained" sx={{background:"#4D6A9A", boxShadow:0, borderRadius:'20px'}}>
-                <Link to="/katakana">ア</Link>
-            </Button>
-            <Button  variant="contained" sx={{background:"#73683F", boxShadow:0, borderRadius:'20px'}}>
-                <Link to="/playing-card">♥♠</Link>
-            </Button>
-                <Button  variant="contained" sx={{background:"#73683F", boxShadow:0, borderRadius:'20px'}}>
-                    <Link to="/custom">yours</Link>
-                </Button>
-            </Stack>
-            <Button  variant="contained">
-                <Link to="/card-collection">collection</Link>
-            </Button>
+            <GameMenu/>
         </>
     )
 
