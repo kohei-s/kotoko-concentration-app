4e6ef1fe2c10b3eb8b93866348078a721e7a0493
Kohei Suzuki <koheisuzuki@me.com>
2023-08-13 15:26:01 +0200

    Add integration tests for MongoUserController

--- backend/src/test/java/de/neuefische/koheis/backend/security/MongoUserControllerIntegrationTest.java
+++ backend/src/test/java/de/neuefische/koheis/backend/security/MongoUserControllerIntegrationTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,7 +19,9 @@ class MongoUserControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+
     @Test
+    @DirtiesContext
     void getAnonymousUser_whenNotLoggedInUser() throws Exception {
         //WHEN
         mockMvc.perform(MockMvcRequestBuilders.get("/api/users/me"))
@@ -24,5 +30,40 @@ class MongoUserControllerIntegrationTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("anonymousUser"));
     }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username="test", password ="test123")
+    void getName_whenLoggedIn() throws Exception {
+        //WHEN
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/users/login")
+                        .with(httpBasic("test", "test123")).with(csrf()))
+
+                // THEN
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("test"));
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username = "test", password="test123")
+    void getAnonymousUser_whenLoggedOut() throws Exception {
+        //GIVEN
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/users/login")
+                        .with(httpBasic("test", "test123")).with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("test"));
+
+        //WHEN
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/users/logout")
+                .with(csrf()));
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/me"))
+
+                //THEN
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("anonymousUser"));
+
+    }
+
 
 }
