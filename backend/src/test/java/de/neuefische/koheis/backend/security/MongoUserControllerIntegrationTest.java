ef32bce2e8f5f075c328245cc972913568f3d7e0
Kohei Suzuki <koheisuzuki@me.com>
2023-08-14 21:20:52 +0200

    Add update UserInfo in backend

--- backend/src/test/java/de/neuefische/koheis/backend/security/MongoUserControllerIntegrationTest.java
+++ backend/src/test/java/de/neuefische/koheis/backend/security/MongoUserControllerIntegrationTest.java
@@ -1,6 +1,5 @@
 package de.neuefische.koheis.backend.security;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
