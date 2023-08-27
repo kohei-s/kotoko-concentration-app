e1ace01915e19fb3012d10b2acf826528b4e4ace
Kohei Suzuki <koheisuzuki@me.com>
2023-08-27 12:09:17 +0200

    Modify Config and Test for react forwarding in backend

--- backend/src/test/java/de/neuefische/koheis/backend/ConfigTest.java
+++ backend/src/test/java/de/neuefische/koheis/backend/ConfigTest.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 class ConfigTest {
