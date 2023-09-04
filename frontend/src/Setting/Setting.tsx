b31c381b66277542929d7d1e9742ca8e450de640
Kohei Suzuki <koheisuzuki@me.com>
2023-09-04 11:51:38 +0200

    Add modal explanations and select form for game expansion in setting

--- frontend/src/Setting/Setting.tsx
+++ frontend/src/Setting/Setting.tsx
@@ -18,8 +18,6 @@ import {CardSet} from "./CardSet.ts";
 import "./Setting.css"
 
 
-
-
 type Props = {
     userInfo: UserInfo | undefined
     update: (userInfo: UserInfo) => void
@@ -29,14 +27,18 @@ export default function Setting(props: Props) {
 
     const [userData, setUserData] = useState<UserInfo>();
     const [userLevels, setUserLevels] = useState<string[]>([]);
+    const [userDiacritics, setUserDiacritics] = useState<boolean[]>([]);
     const [hiraganaLevel, setHiraganaLevel] = useState<string>("small");
     const [katakanaLevel, setKatakanaLevel] = useState<string>("small");
     const [playingCardsLevel, setPlayingCardsLevel] = useState<string>("small");
     const [customLevel, setCustomLevel] = useState<string>("small");
     const [selectedCustomGame, setSelectedCustomGame] = useState<string>("");
     const [countCardSets, setCountCardSets] = useState<CardSet[]>([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
+    const [hiraganaDiacritics, setHiraganaDiacritics] = useState<boolean>(false);
+    const [katakanaDiacritics, setKatakanaDiacritics] = useState<boolean>(false);
+    const [isCustomGameModalOpen, setIsCustomGameModalOpen] = useState(false);
+    const [isLevelsModalOpen, setIsLevelsModalOpen] = useState(false);
+    const [isDiacriticsModalOpen, setIsDiacriticsModalOpen] = useState(false);
 
     useEffect(() => {
         setCountCardSets(props.countCardSets)
@@ -44,6 +46,7 @@ export default function Setting(props: Props) {
         if (props.userInfo) {
             setUserLevels(props.userInfo.levels)
             setSelectedCustomGame(props.userInfo.selectedCardSet)
+            setUserDiacritics(props.userInfo.diacritics)
         }
     }, [props.userInfo, props.countCardSets]);
 
@@ -65,6 +68,22 @@ export default function Setting(props: Props) {
 
     const changeSelectedCustomGame = (event: SelectChangeEvent) => {
         setSelectedCustomGame(event.target.value)
+    }
+
+    const changeHiraganaDiacritics = (event: SelectChangeEvent) => {
+        if (event.target.value === "on") {
+            setHiraganaDiacritics(true)
+        } else {
+            setHiraganaDiacritics(false)
+        }
+    }
+
+    const changeKatakanaDiacritics = (event: SelectChangeEvent) => {
+        if (event.target.value === "on") {
+            setKatakanaDiacritics(true)
+        } else {
+            setKatakanaDiacritics(false)
+        }
     }
 
     function setUserLevelText(index: number) {
@@ -105,14 +124,52 @@ export default function Setting(props: Props) {
 
     }
 
-    function openModal() {
-        setIsModalOpen(true)
+    function setUserDiacriticsText(index: number) {
+        switch (userDiacritics[index]) {
+            case true:
+                return "on";
+            default:
+                return "off";
+        }
     }
 
-    function closeModal() {
-        setIsModalOpen(false)
+    function updateDiacritics() {
+        const newDiacritics: boolean[] = [hiraganaDiacritics, katakanaDiacritics]
+        setUserDiacritics(newDiacritics)
+        const updatedUserInfo: UserInfo = {
+            username: userData?.username as string,
+            achievement: userData?.achievement as string,
+            wordbook: userData?.wordbook as string[],
+            diacritics: newDiacritics,
+            levels: userData?.levels as string[],
+            selectedCardSet: userData?.selectedCardSet as string
+        }
+        props.update(updatedUserInfo)
     }
 
+    function openCustomGameModal() {
+        setIsCustomGameModalOpen(true)
+    }
+
+    function closeCustomGameModal() {
+        setIsCustomGameModalOpen(false)
+    }
+
+    function openLevelsModal() {
+        setIsLevelsModalOpen(true)
+    }
+
+    function closeLevelsModal() {
+        setIsLevelsModalOpen(false)
+    }
+
+    function openDiacriticsModal() {
+        setIsDiacriticsModalOpen(true)
+    }
+
+    function closeDiacriticsModal() {
+        setIsDiacriticsModalOpen(false)
+    }
 
     return (
         <>
@@ -123,17 +180,17 @@ export default function Setting(props: Props) {
             <div className={"selector"}>
                 <h3>Custom Game
                     <IconButton disableRipple={true} size="small" className={"buttonAdd"}
-                                onClick={openModal} sx={{color: "#a5a7d7", boxShadow: 0}}>
+                                onClick={openCustomGameModal} sx={{color: "#a5a7d7", boxShadow: 0}}>
                         <HelpOutlineIcon fontSize={"small"}/>
                     </IconButton>
                 </h3>
                 <Modal
                     className={"modal-message"}
-                    open={isModalOpen}
+                    open={isCustomGameModalOpen}
                     sx={{mt: 10, ml: 4, mr: 4, marginBottom: 35, backgroundColor: "#ffffff"}}
                 >
                     <Box>
-                        <Button id={"close-modal"} onClick={closeModal}
+                        <Button id={"close-modal"} onClick={closeCustomGameModal}
                                 sx={{
                                     mt: 5,
                                     mb: 2,
@@ -151,19 +208,17 @@ export default function Setting(props: Props) {
                     </Box>
                 </Modal>
                 <FormControl sx={{m: 1, width: 225, textAlign: "center"}}>
-                    <InputLabel id="Custom-game">Card Set</InputLabel>
                     <Select
                         labelId="Custom-game"
                         id="Custom-game"
                         value={selectedCustomGame}
-                        label="Card set"
                         onChange={changeSelectedCustomGame}
                     >
                         {countCardSets.filter(setName => setName.count > 4)
                             .map(setName => <MenuItem
-                            key={setName.name}
-                            value={setName.name}
-                        >{setName.name} ({setName.count})</MenuItem>)}
+                                key={setName.name}
+                                value={setName.name}
+                            >{setName.name} ({setName.count})</MenuItem>)}
                     </Select>
                     <FormHelperText>current set: {selectedCustomGame}</FormHelperText>
                 </FormControl>
@@ -180,7 +235,34 @@ export default function Setting(props: Props) {
                 </IconButton>
             </div>
             <div className={"selector"}>
-                <h3 id={"level"}>Game Level</h3>
+                <h3 id={"entry"}>Game Level
+                    <IconButton disableRipple={true} size="small" className={"buttonAdd"}
+                                onClick={openLevelsModal} sx={{color: "#a5a7d7", boxShadow: 0}}>
+                        <HelpOutlineIcon fontSize={"small"}/>
+                    </IconButton>
+                </h3>
+                <Modal
+                    className={"modal-level"}
+                    open={isLevelsModalOpen}
+                    sx={{mt: 10, ml: 4, mr: 4, marginBottom: 35, backgroundColor: "#ffffff"}}
+                >
+                    <Box>
+                        <Button id={"close-modal"} onClick={closeLevelsModal}
+                                sx={{
+                                    mt: 5,
+                                    mb: 2,
+                                    color: "#ffffff",
+                                    boxShadow: 0
+                                }}><CancelIcon/></Button>
+                        <Typography sx={{ml: 3, color: "#ffffff"}}>
+                            Three levels of game
+                        </Typography>
+                        <Typography sx={{mr: 3, ml: 3, mt: 2, color: "#ffffff"}}>
+                            Concentration with 3x3 cards for beginner, 3X4 cards for intermediate and 4x4 cards for
+                            advanced player.
+                        </Typography>
+                    </Box>
+                </Modal>
                 <FormControl sx={{m: 1, width: 225, textAlign: "center"}}>
                     <InputLabel id="Hiragana-level">HIRAGANA</InputLabel>
                     <Select
@@ -190,9 +272,9 @@ export default function Setting(props: Props) {
                         label="Hiargana"
                         onChange={changeHiraganaLevel}
                     >
-                        <MenuItem value={"small"}>Beginner (3x3)</MenuItem>
-                        <MenuItem value={"medium"}>Intermediate (3x4)</MenuItem>
-                        <MenuItem value={"large"}>Advanced (4x4)</MenuItem>
+                        <MenuItem value={"small"}>Beginner</MenuItem>
+                        <MenuItem value={"medium"}>Intermediate</MenuItem>
+                        <MenuItem value={"large"}>Advanced</MenuItem>
                     </Select>
                     <FormHelperText>current level: {setUserLevelText(0)}</FormHelperText>
                 </FormControl>
@@ -205,9 +287,9 @@ export default function Setting(props: Props) {
                         label="Katakana"
                         onChange={changeKatakanaLevel}
                     >
-                        <MenuItem value={"small"}>Beginner (3x3)</MenuItem>
-                        <MenuItem value={"medium"}>Intermediate (3x4)</MenuItem>
-                        <MenuItem value={"large"}>Advanced (4x4)</MenuItem>
+                        <MenuItem value={"small"}>Beginner</MenuItem>
+                        <MenuItem value={"medium"}>Intermediate</MenuItem>
+                        <MenuItem value={"large"}>Advanced</MenuItem>
                     </Select>
                     <FormHelperText>current level: {setUserLevelText(1)}</FormHelperText>
                 </FormControl>
@@ -220,9 +302,9 @@ export default function Setting(props: Props) {
                         label="PlayingCards"
                         onChange={changePlayingCardsLevel}
                     >
-                        <MenuItem value={"small"}>Beginner (3x3)</MenuItem>
-                        <MenuItem value={"medium"}>Intermediate (3x4)</MenuItem>
-                        <MenuItem value={"large"}>Advanced (4x4) </MenuItem>
+                        <MenuItem value={"small"}>Beginner</MenuItem>
+                        <MenuItem value={"medium"}>Intermediate</MenuItem>
+                        <MenuItem value={"large"}>Advanced</MenuItem>
                     </Select>
                     <FormHelperText>current level: {setUserLevelText(2)}</FormHelperText>
                 </FormControl>
@@ -235,9 +317,9 @@ export default function Setting(props: Props) {
                         label="Custom"
                         onChange={changeCustomLevel}
                     >
-                        <MenuItem value={"small"}>Beginner (3x3)</MenuItem>
-                        <MenuItem value={"medium"}>Intermediate (3x4)</MenuItem>
-                        <MenuItem value={"large"}>Advanced (4x4)</MenuItem>
+                        <MenuItem value={"small"}>Beginner</MenuItem>
+                        <MenuItem value={"medium"}>Intermediate</MenuItem>
+                        <MenuItem value={"large"}>Advanced</MenuItem>
                     </Select>
                     <FormHelperText>current
                         level: {setUserLevelText(3)}</FormHelperText>
@@ -247,10 +329,78 @@ export default function Setting(props: Props) {
                     color: "#FDF6E1",
                     background: "#508356",
                     boxShadow: 0,
+                    borderRadius: '10px'
+                }} onClick={() => {
+                    updateLevels()
+                }}>
+                    <CheckIcon/>
+                </IconButton>
+            </div>
+            <div className={"selector"}>
+                <h3 id={"entry"}>Expansion cards
+                    <IconButton disableRipple={true} size="small" className={"buttonAdd"}
+                                onClick={openDiacriticsModal} sx={{color: "#a5a7d7", boxShadow: 0}}>
+                        <HelpOutlineIcon fontSize={"small"}/>
+                    </IconButton>
+                </h3>
+                <Modal
+                    className={"modal-diacritics"}
+                    open={isDiacriticsModalOpen}
+                    sx={{mt: 10, ml: 4, mr: 4, marginBottom: 35, backgroundColor: "#ffffff"}}
+                >
+                    <Box>
+                        <Button id={"close-modal"} onClick={closeDiacriticsModal}
+                                sx={{
+                                    mt: 5,
+                                    mb: 2,
+                                    color: "#ffffff",
+                                    boxShadow: 0
+                                }}><CancelIcon/></Button>
+                        <Typography sx={{ml: 3, color: "#ffffff"}}>
+                            Diacritics of Hiragana and Katakana
+                        </Typography>
+                        <Typography sx={{mr: 3, ml: 3, mt: 2, color: "#ffffff"}}>
+                            You can expand your knowledge of Hiragana and Katakana with diacritics cards.
+                        </Typography>
+                    </Box>
+                </Modal>
+                <FormControl sx={{m: 1, width: 225, textAlign: "center"}}>
+                    <InputLabel id="Hiragana-diacritics">HIRAGANA</InputLabel>
+                    <Select
+                        labelId="Hiragana-diacritics"
+                        id="Hiragana-diacritics"
+                        value={(hiraganaDiacritics)? "on":"off"}
+                        label="Hiargana"
+                        onChange={changeHiraganaDiacritics}
+                    >
+                        <MenuItem value={"on"}>ON</MenuItem>
+                        <MenuItem value={"off"}>OFF</MenuItem>
+                    </Select>
+                    <FormHelperText>current setting: {setUserDiacriticsText(0)}</FormHelperText>
+                </FormControl>
+                <FormControl sx={{m: 1, width: 225, textAlign: "center"}}>
+                    <InputLabel id="Katakana-diacritics">KATAKANA</InputLabel>
+                    <Select
+                        labelId="Katakana-diacritics"
+                        id="Katakana-diacritics"
+                        value={(katakanaDiacritics)? "on":"off"}
+                        label="Katakana"
+                        onChange={changeKatakanaDiacritics}
+                    >
+                        <MenuItem value={"on"}>ON</MenuItem>
+                        <MenuItem value={"off"}>OFF</MenuItem>
+                    </Select>
+                    <FormHelperText>current setting: {setUserDiacriticsText(1)}</FormHelperText>
+                </FormControl>
+                <IconButton id={"save"} size="small" disableRipple={true} sx={{
+                    maxWidth: 60,
+                    color: "#FDF6E1",
+                    background: "#508356",
+                    boxShadow: 0,
                     borderRadius: '10px',
                     marginBottom: '50px'
                 }} onClick={() => {
-                    updateLevels()
+                    updateDiacritics()
                 }}>
                     <CheckIcon/>
                 </IconButton>
