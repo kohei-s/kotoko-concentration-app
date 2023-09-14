ae72091506fe38b68167e3088c4df904ad074c16
Kohei Suzuki <koheisuzuki@me.com>
2023-09-14 22:53:09 +0200

    Remove test from DeepLServiceTest

--- backend/src/test/java/de/neuefische/koheis/backend/translation/DeepLServiceTest.java
+++ backend/src/test/java/de/neuefische/koheis/backend/translation/DeepLServiceTest.java
@@ -1,12 +1,7 @@
 package de.neuefische.koheis.backend.translation;
 
-import com.deepl.api.DeepLException;
-import com.deepl.api.Translator;
-import com.deepl.api.TranslatorOptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.HashMap;
-import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
@@ -14,7 +9,6 @@ import static org.mockito.Mockito.*;
 
 class DeepLServiceTest {
     DeepLConfig deepLConfig = mock(DeepLConfig.class);
-    Translator translator;
     DeepLService deepLService;
 
     @BeforeEach
@@ -46,30 +40,6 @@ class DeepLServiceTest {
 
         //THEN
         assertThrows(Exception.class, () -> deepLService.getJapaneseTranslation(originalText, originalLanguage));
-    }
-
-    @Test
-    void expectTranslation_whenTranslateTextCalled() throws DeepLException, InterruptedException {
-        //GIVEN
-        String originalWord = "proton beam";
-        String translatedWord = "陽子ビーム";
-        String originalLanguage = "EN";
-        String authKey = "test:fx";
-        Map<String, String> headers = new HashMap<>();
-        headers.put("mock-server-session-allow-missing-user-agent", "5");
-        headers.put("mock-server-session-allow-reconnections", "5");
-        Translation expected = new Translation(originalWord, translatedWord);
-        TranslatorOptions options = new TranslatorOptions()
-                .setServerUrl("http://localhost:3000")
-                .setHeaders(headers);
-
-        //WHEN
-        when(deepLConfig.getKey()).thenReturn(authKey);
-        translator = new Translator(authKey, options);
-        Translation actual = new Translation(originalWord, translator.translateText(originalWord, originalLanguage, "JA").getText());
-
-        //THEN
-        assertEquals(expected, actual);
     }
 
 }
