52529dd3cfb2500fd1bcf0f24795ec806ab25c4f
Kohei Suzuki <koheisuzuki@me.com>
2023-10-16 11:15:37 +0200

    Add ConvertServiceTest and CovertIntegrationTest

--- backend/src/test/java/de/neuefische/koheis/backend/converter/ConvertIntegrationTest.java
+++ backend/src/test/java/de/neuefische/koheis/backend/converter/ConvertIntegrationTest.java
@@ -1,9 +1,6 @@
 package de.neuefische.koheis.backend.converter;
 
 import de.neuefische.koheis.backend.translation.Translation;
-import okhttp3.mockwebserver.MockResponse;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import okhttp3.mockwebserver.MockWebServer;
-
-import java.io.IOException;
-
-import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
