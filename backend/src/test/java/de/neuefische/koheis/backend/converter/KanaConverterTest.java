4c011ac611dd3b2dae554af9c53df9cc8bcfdab3
Kohei Suzuki <koheisuzuki@me.com>
2023-11-07 16:20:53 +0100

    Modify KanaConverterTest in backend to resolve sonar warnings

--- backend/src/test/java/de/neuefische/koheis/backend/converter/KanaConverterTest.java
+++ backend/src/test/java/de/neuefische/koheis/backend/converter/KanaConverterTest.java
@@ -1,35 +1,39 @@
 package de.neuefische.koheis.backend.converter;
-
 import com.ibm.icu.text.Transliterator;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.mockito.Mockito;
 
-public class KanaConverterTest {
+@ExtendWith(MockitoExtension.class)
+class KanaConverterTest {
+
     private KanaConverter kanaConverter;
 
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
+    @Mock
+    private Transliterator transliterator;
+
+    @BeforeEach
+    void setUp() {
         kanaConverter = new KanaConverter();
     }
 
     @Test
-    public void testConvertHiraganaToAlphabet() {
+    void testConvertHiraganaToAlphabet() {
         String hiragana = "ひらがな";
-        Transliterator trans = Transliterator.getInstance("Hiragana-Latin");
-        String expected = trans.transliterate(hiragana);
+        Mockito.lenient().when(transliterator.transliterate(hiragana)).thenReturn("hiragana");
         String result = kanaConverter.convertHiraganaToAlphabet(hiragana);
-        assertEquals(expected, result);
+        assertEquals("hiragana", result);
     }
 
     @Test
-    public void testConvertKatakanaToAlphabet() {
+    void testConvertKatakanaToAlphabet() {
         String katakana = "カタカナ";
-        Transliterator trans = Transliterator.getInstance("Katakana-Latin");
-        String expected = trans.transliterate(katakana);
+        Mockito.lenient().when(transliterator.transliterate(katakana)).thenReturn("katakana");
         String result = kanaConverter.convertKatakanaToAlphabet(katakana);
-        assertEquals(expected, result);
+        assertEquals("katakana", result);
     }
 }
